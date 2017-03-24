bcc196bde8d01dfa2f6934091839c7356e91fd03
Stefan Britton <stefan.britton@me.com>
2017-03-23 21:44:45 -0500

    Initial Commit

---
+++ SBLineView/SBLineView.h
@@ -0,0 +1,19 @@
+//
+//  SBLineView.h
+//  SBLineView
+//
+//  Created by Stefan Britton on 2017-03-23.
+//  Copyright © 2017 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBLineView.
+FOUNDATION_EXPORT double SBLineViewVersionNumber;
+
+//! Project version string for SBLineView.
+FOUNDATION_EXPORT const unsigned char SBLineViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBLineView/PublicHeader.h>
+
+
